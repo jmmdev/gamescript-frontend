fb327e2773c0ad3043ce6b4e783cf9aeec89f241
unknown <devjosm@gmail.com>
2024-08-15 14:50:15 +0200

    Bug fixes

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import Footer from "./components/footer";
 
 export default function Home() {
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+    console.log(BASE_URL);
 
     const [data, setData] = useState(null);
 
