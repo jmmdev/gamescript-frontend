c68ac7f8b7296fe7df28e3cd87f5f411b95f8975
unknown <devjosm@gmail.com>
2024-12-06 16:48:44 +0100

    Initial Tailwind changes

--- src/app/components/game-highlight.js
+++ src/app/components/game-highlight.js
@@ -111,7 +111,7 @@ export default function GameHighlight() {
                 <>
                     {screenshots[activeGame]}
                     <div className="absolute w-full h-full bg-gradient-to-b from-transparent to-gray-900 to-98%" />
-                    <div className="w-full absolute top-16 flex flex-col justify-end h-[calc(min(100vw_*_9_/_16,_100vh)_-_4rem)] ">
+                    <div className="w-full absolute top-16 flex flex-col justify-end h-[calc(min(100vw_*_9_/_16,_100vh)_-_4rem)] z-30">
                         <div className={styles['highlight-navigation']}>
                             <button className={styles['highlight-navigation-button']} onClick={() => setActiveGame(activeGame > 0 ? activeGame-1 : randomGames.length-1)}>
                                 <FaChevronLeft />
