df5448cbaf8b5eb4765cddc9e08606261889fa9e
unknown <devjosm@gmail.com>
2024-12-08 00:40:13 +0100

    More Tailwind changes

--- src/app/components/game-highlight.js
+++ src/app/components/game-highlight.js
@@ -60,7 +60,7 @@ export default function GameHighlight() {
 
       const GetGameDetails = ({game, setShowGameDetails}) => {
         return (
-            <div className="w-full lg:w-3/5 h-1/2 flex items-center justify-between sm:flex-col sm:items-start sm:justify-center gap-4 p-4 pt-0 ">
+            <div className="w-full lg:w-3/5 h-1/2 flex items-center justify-between sm:flex-col sm:items-start sm:justify-center gap-4 px-[2.5%] pt-0 ">
                 <div className="w-full flex flex-col">
                     <p className="text-2xl sm:text-4xl font-bold text-white">{game.name}</p>
                     <p className="hidden lg:line-clamp-2 truncate whitespace-normal text-xl text-white text-justify">{game.summary}</p>
