994773dba95c9f0bc6435bf2ee10a47432091c8d
unknown <devjosm@gmail.com>
2024-12-08 13:00:55 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import styles from '../page.module.css';
 import Image from "next/image";
 import Link from "next/link";
 import {IoMenu} from 'react-icons/io5';
@@ -7,8 +6,6 @@ import {IoMenu} from 'react-icons/io5';
 export default function Header({isDynamic}) {
     let threshold = 0;
     const [opacityRatio, setOpacityRatio] = useState(0);
-
-    const [layout, setLayout] = useState(0);
     const [showMenu, setShowMenu] = useState(false);
 
     useEffect(() => {
@@ -22,7 +19,6 @@ export default function Header({isDynamic}) {
         }
 
         function updateLayout() {
-            setLayout(window.innerWidth >= 380 ? 0 : 1)
             threshold = window.innerWidth * 9 / 16 - 64
         }
 
@@ -37,30 +33,14 @@ export default function Header({isDynamic}) {
     }, [])
 
     const GetLayout = () => {
-        if (layout === 1)
-            return (
-                <div className="w-full flex justify-between items-center gap-1">
-                    <Link className="relative w-24 aspect-[4.875]" href={{pathname: '/'}}>
-                        <Image src={'/assets/logo.png'} fill alt="alt-logo.png" />
-                    </Link>
-                    <button className="text-3xl text-gray-300" onClick={() => setShowMenu(!showMenu)}>
-                        <IoMenu />
-                    </button>
-                </div>
-            )
         return (
-            <div className="flex items-center gap-6">
-                <Link className="relative w-48 aspect-[4.875]" href={{pathname: '/'}}>
-                    <Image src={'/assets/logo.png'} fill alt="logo.png" />
+            <div className="w-full flex justify-between items-center gap-1">
+                <Link className="relative w-24 aspect-[4.875]" href={{pathname: '/'}}>
+                    <Image src={'/assets/logo.png'} fill alt="alt-logo.png" />
                 </Link>
-                <div className="flex gap-4 items-center">
-                    <Link className="text-sm text-white font-bold hover:text-[#dd202d]" href={{pathname: '/genres'}}>
-                        <p>Genres</p>
-                    </Link>
-                    <Link className="text-sm text-white font-bold hover:text-[#dd202d]" href={{pathname: '/themes'}}>
-                        <p>Themes</p>
-                    </Link>
-                </div>
+                <button className="text-3xl text-gray-300" onClick={() => setShowMenu(!showMenu)}>
+                    <IoMenu />
+                </button>
             </div>
         )
     }
@@ -71,15 +51,15 @@ export default function Header({isDynamic}) {
     * 
     */
   
-    const GetHeaderMenu = () => {
+    const GetSideMenu = () => {
         if (showMenu) {
             return (
-                <div className="fixed top-0 left-0 w-screen h-screen md:w-[200px] bg-gray-800 z-50 flex flex-col justify-center items-center gap-4">
-                    <Link className="no-underline" href={{pathname: '/genres'}}>
-                        <p className="text-sm text-white font-bold text-center">Genres</p>
+                <div className="fixed top-0 left-0 w-full h-screen lg:w-[25%] bg-gray-800 z-50 flex flex-col justify-center items-center gap-4">
+                    <Link className="no-underline hover:underline" href={{pathname: '/genres'}}>
+                        <p className="text-sm text-white font-medium text-center">Genres</p>
                     </Link>
-                    <Link className="no-underline" href={{pathname: '/themes'}}>
-                        <p className="text-sm text-white font-bold text-center">Themes</p>
+                    <Link className="no-underline hover:underline" href={{pathname: '/themes'}}>
+                        <p className="text-sm text-white font-medium text-center">Themes</p>
                     </Link>
                 </div>
             )
@@ -93,7 +73,7 @@ export default function Header({isDynamic}) {
         style={{background: isDynamic ? `linear-gradient(0deg, rgba(17,24,39,${opacityRatio}) 0%, rgba(3,7,18,1) 100%)` : `linear-gradient(0deg, rgba(17,24,39,1) 0%, rgba(3,7,18,1) 100%)`}}>
             <GetLayout />
         </div>
-        <GetHeaderMenu />
+        <GetSideMenu />
         </>
     )
 }
