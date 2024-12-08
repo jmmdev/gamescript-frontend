2f96c7704c59cdfa0b8af0c32a96161e142bfce7
unknown <devjosm@gmail.com>
2024-12-08 21:51:29 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -44,7 +44,7 @@ export default function Header({isDynamic}) {
 
     return (
         <>
-        <div className={`flex justify-between items-center h-16 w-full fixed top-0 p-4 z-30 ${isDynamic ? `bg-gradient-to-t from-[rgba(17,24,39,${opacityRatio}] to-gray-950` : "bg-gradient-to-t from-gray-900 to-gray-950"}`}>
+        <div className={`flex justify-between items-center h-16 w-full fixed top-0 p-4 z-30 bg-gradient-to-t to-gray-950 from-${isDynamic ? `[rgba(17,24,39,${opacityRatio}]` : "gray-900"}`}>
             <div className="w-full flex justify-between items-center gap-1">
                 <Link className="relative w-48 aspect-[4.875]" href={{pathname: '/'}}>
                     <Image src={'/assets/logo.png'} fill alt="alt-logo.png" />
