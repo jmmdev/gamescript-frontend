67a87c9f4bf5d25034c13a24c736ba78f48e6e8b
unknown <devjosm@gmail.com>
2024-12-08 22:53:37 +0100

    More Tailwind changes

--- src/app/page.js
+++ src/app/page.js
@@ -23,19 +23,6 @@ export default function Home() {
                 return null;
             }
         }
-        async function clone() {
-            try {
-                const response = await fetch(`${BASE_URL}/clone`,
-                {
-                method: 'GET'
-                });
-                const text = await response.text();
-                console.log(text);
-            } catch(e) {
-                return null;
-            }
-        }
-        clone();
         getData();
     }, [])
 
