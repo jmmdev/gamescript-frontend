d052fe8a60a3818b1a7cd1b4bbc34fe98e1897bb
unknown <devjosm@gmail.com>
2024-12-08 14:02:38 +0100

    More Tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
     extend: {
       animation: {
         fade: 'fadeIn 0.3s linear',
+        slide: 'slide 0.3 linear forwards'
       },
       keyframes: () => ({
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 0.85 },
         },
+        slide: {
+          '0%': { left: '-250px' },
+          '100%': { left: 0 },
+        }
       }),
       gradientColorStopPositions: {
         98: '98%',
