f9a95e76bc0e6bf2992cfc6ad93fd746cab211a1
unknown <devjosm@gmail.com>
2024-12-08 01:08:09 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -74,7 +74,7 @@ export default function Header({isDynamic}) {
     const GetHeaderMenu = () => {
         if (showMenu) {
             return (
-                <div className="w-screen h-screen bg-gray-900 z-50 flex flex-col items-center gap-4">
+                <div className="absolute top-0 left-0 w-screen h-screen md:w-[500px] bg-gray-900 z-50 flex flex-col items-center gap-4">
                     <Link className={styles['header-button']} href={{pathname: '/genres'}}>
                         <p>Genres</p>
                     </Link>
