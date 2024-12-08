29ee1e138cff015779abe4918ab05ed05ab046dd
unknown <devjosm@gmail.com>
2024-12-08 15:14:34 +0100

    More Tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,22 @@ module.exports = {
     extend: {
       animation: {
         fade: 'fadeIn 0.2s linear',
-        'slide-left': 'slideLeft 0.2s linear',
-        'hide-left': 'hideLeft 0.2s linear',
       },
       keyframes: () => ({
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 0.85 },
         },
-        slideLeft: {
-          '0%': { left: '-250px' },
-          '100%': { left: 0 },
-        },
-        hideLeft: {
-          '0%': { left: 0 },
-          '100%': { left: '-250px' },
-        }
       }),
       gradientColorStopPositions: {
         98: '98%',
       },
       fontFamily: {
         sans: ['var(--my-font)'],
-      }
+      },
+      transitionProperty: {
+        'width': 'width',
+      },
     },
   },
   plugins: [],
