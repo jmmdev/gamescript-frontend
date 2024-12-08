736f12c6d9b0640106dd1a09cbda8a7099d55049
unknown <devjosm@gmail.com>
2024-12-08 14:43:48 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -53,13 +53,13 @@ export default function Header({isDynamic}) {
   
     const GetSideMenu = () => {
         return (
-            <div className={`fixed top-0 w-full h-screen md:left-[-250px] md:${showMenu ? "animate-slide-left" : "animate-hide-left"} md:w-[250px] bg-gray-800 z-50 flex flex-col justify-between items-center gap-4 p-3`}>
-                <div className="w-full flex items-center justify-end">
+            <div className={`fixed top-0 h-screen w-full md:w-[250px] ${showMenu ? "grid-rows-1 md:grid-cols-1" : "grid-rows-none md:grid-cols-none"} ease-in-out bg-gray-800 z-50 grid gap-4 p-3`}>
+                <div className="w-full flex items-center justify-end overflow-hidden">
                     <button className="text-gray-300 hover:text-gray-400 active:text-white" onClick={() => setShowMenu(false)}>
                         <IoClose className="text-2xl" />
                     </button>
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-3 overflow-hidden">
                     <Link className="no-underline hover:underline" href={{pathname: '/genres'}}>
                         <p className="text-sm text-white font-medium text-center">Genres</p>
                     </Link>
@@ -67,7 +67,7 @@ export default function Header({isDynamic}) {
                         <p className="text-sm text-white font-medium text-center">Themes</p>
                     </Link>
                 </div>
-                <div>
+                <div className="overflow-hidden">
                     Footer
                 </div>
             </div>
