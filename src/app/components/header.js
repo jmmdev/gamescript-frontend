68d8deae25bafdd451fec9429b32ded3996384d1
unknown <devjosm@gmail.com>
2024-12-09 15:45:41 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -96,7 +96,7 @@ export default function Header({isDynamic}) {
         })
 
         return (
-            <div className="w-full flex flex-col gap-3 px-8">
+            <div className="w-full flex flex-col gap-3 px-8 sm:pb-4">
                 <div className="w-fit flex flex-col gap-3">
                     <p className="text-lg text-white font-bold uppercase">Genres</p>
                     <div className="flex flex-col gap-2">
@@ -127,19 +127,14 @@ export default function Header({isDynamic}) {
                     }
                 </div>
             </div>
-            {showMenu &&
-                <div id="menu" className="fixed w-full h-screen top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 overflow-y-auto flex flex-col justify-between items-center sm:w-fit sm:py-4 translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
-                    <div className="w-full flex sm:hidden justify-end items-center p-4">
-                        <button className="text-3xl text-gray-300 hover:text-white active:text-gray-400" onClick={() => setShowMenu(false)}>
-                            <IoClose />
-                        </button>
-                    </div>
-                    {categories && <GetCategories />}
-                    <div className="w-full p-4">
-                        Footer
-                    </div>
+            <div id="menu" className={`fixed w-full h-screen top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 overflow-y-auto flex flex-col justify-between items-center sm:w-fit sm:py-4 translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0 ${showMenu ? "visible" : "invisible"}`}>
+                <div className="w-full flex sm:hidden justify-end items-center p-4">
+                    <button className="text-3xl text-gray-300 hover:text-white active:text-gray-400" onClick={() => setShowMenu(false)}>
+                        <IoClose />
+                    </button>
                 </div>
-            }
+                {categories && <GetCategories />}
+            </div>
         </>
     )
 }
