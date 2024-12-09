338ff98b0882754041ecadaeb35cd01615581cbf
unknown <devjosm@gmail.com>
2024-12-09 04:02:24 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ import {IoClose, IoMenu} from 'react-icons/io5';
 
 export default function Header({isDynamic}) {
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-    
+
     const threshold = useRef(0);
     const [opacityRatio, setOpacityRatio] = useState(0);
     const [showMenu, setShowMenu] = useState(false);
@@ -83,10 +83,10 @@ export default function Header({isDynamic}) {
 
         return (
             <div>
-            <p>Genres</p>
-            {genres}
-            <p>Themes</p>
-            {themes}
+                <p>Genres</p>
+                {genres}
+                <p>Themes</p>
+                {themes}
             </div>
         )
     }
@@ -98,9 +98,11 @@ export default function Header({isDynamic}) {
                     <Link className="relative w-48 aspect-[4.875]" href={{pathname: '/'}}>
                         <Image src={'/assets/logo.png'} fill alt="alt-logo.png" />
                     </Link>
-                    <button className={`text-3xl text-gray-300 hover:text-white active:text-gray-400 ${categories ? "block" : "hidden"}`} onClick={() => setShowMenu(!showMenu)}>
-                        <IoMenu />
-                    </button>
+                    {categories &&
+                        <button className={`text-3xl text-gray-300 hover:text-white active:text-gray-400 ${categories ? "block" : "hidden"}`} onClick={() => setShowMenu(!showMenu)}>
+                            <IoMenu />
+                        </button>
+                    }
                 </div>
             </div>
             <div id="menu" className="fixed w-full top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 flex flex-col justify-between items-center gap-4 p-4 sm:w-[14rem] sm:h-screen translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
