f3781849812417a49c87cfba6e1f4b56f53d4dbd
unknown <devjosm@gmail.com>
2024-12-09 15:37:01 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -119,14 +119,14 @@ export default function Header({isDynamic}) {
                     }
                 </div>
             </div>
-            <div id="menu" className="fixed w-full h-screen top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 overflow-y-auto flex flex-col justify-between items-center sm:w-fit translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
+            <div id="menu" className="fixed w-full h-screen top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 overflow-y-auto flex flex-col justify-between items-center sm:w-fit sm:py-4 translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
                 <div className="w-full flex sm:hidden justify-end items-center p-4">
                     <button className="text-3xl text-gray-300 hover:text-white active:text-gray-400" onClick={() => setShowMenu(false)}>
                         <IoClose />
                     </button>
                 </div>
                 {categories && <GetCategories />}
-                <div className="p-4">
+                <div className="w-full p-4">
                     Footer
                 </div>
             </div>
