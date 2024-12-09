098d1a520c8d59cf2d3711e099ad6ba5891da053
unknown <devjosm@gmail.com>
2024-12-09 01:54:44 +0100

    More Tailwind changes

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -109,21 +109,19 @@ export default function Header({isDynamic}) {
                 }
             </div>
         </div>
-        {categories &&
-            <div id="menu" className="fixed w-full top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 flex flex-col justify-between items-center gap-4 p-4 sm:w-[14rem] sm:h-screen translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
-                <div className="w-full flex sm:hidden justify-end items-center">
-                    <button className="text-3xl text-gray-300 hover:text-white active:text-gray-400" onClick={() => setShowMenu(false)}>
-                        <IoClose />
-                    </button>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <GetCategories />
-                </div>
-                <div>
-                    Footer
-                </div>
+        <div id="menu" className="fixed w-full top-0 left-0 duration-200 ease-in-out bg-gray-800 z-40 flex flex-col justify-between items-center gap-4 p-4 sm:w-[14rem] sm:h-screen translate-x-0 -translate-y-full sm:-translate-x-full sm:translate-y-0">
+            <div className="w-full flex sm:hidden justify-end items-center">
+                <button className="text-3xl text-gray-300 hover:text-white active:text-gray-400" onClick={() => setShowMenu(false)}>
+                    <IoClose />
+                </button>
             </div>
-        }
+            <div className="flex flex-col gap-3">
+                <GetCategories />
+            </div>
+            <div>
+                Footer
+            </div>
+        </div>
         </>
     )
 }
