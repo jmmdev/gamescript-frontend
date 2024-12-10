8d5f03b8be81013137c26605d496ef19074fb710
unknown <devjosm@gmail.com>
2024-12-10 01:45:04 +0100

    More Tailwind changes

--- src/app/page.js
+++ src/app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
         return (
             <div className="flex flex-col p-4 px-[2.5%] animate-pulse">
                 <div className="h-[2.5rem] sm:h-[3.5rem] rounded-full bg-zinc-700"/>
-                <div className={`w-full h-[${window.innerWidth * 0.4}px] flex bg-zinc-700`} />
+                <div className={`w-full h-[40vw] flex bg-zinc-700`} />
             </div>
         )
     }
